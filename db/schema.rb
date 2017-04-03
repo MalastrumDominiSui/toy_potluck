0e1c3233982bc8549aa17094c3005ec0e7e7c028
Michael Sefranek <msefran@gmail.com>
2017-04-03 16:37:32 -0500

    added items scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403213335) do
+ActiveRecord::Schema.define(version: 20170403213552) do
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.string   "category"
+    t.string   "amount"
+    t.string   "claimed"
+    t.integer  "potluck_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "potlucks", force: :cascade do |t|
     t.string   "name"
